adae7150fe7cde0cf79e9af0b16f17939c90b0e0
Fer <ferchuiviilacis@gmail.com>
2026-06-27 18:58:23 -0300

    Login: estructura inicial

---
+++ TPC-Grupo17A-WebApplicationClinica/dominio/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public enum TipoUsuario //Enum sirve para identificar el tipo de usuario sin usar numeros sueltos en el codigo
+    {
+        Administrador = 1,
+        Recepcionista = 2,
+        Medico = 3
+    }
+
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string User { get; set; }
+        public string Pass { get; set; }
+        public TipoUsuario TipoUsuario { get; set; }
+    }
+}
---
+++ TPC-Grupo17A-WebApplicationClinica/negocio/UsuarioNegocio.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dominio;
+
+namespace negocio
+{
+    public class UsuarioNegocio
+    {
+        public bool Loguear(Usuario usuario)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("SELECT Id, TipoUser FROM Usuarios WHERE Usuario = @user AND Pass = @pass");
+                datos.setearParametro("@user", usuario.User);
+                datos.setearParametro("@pass", usuario.Pass);
+
+                datos.ejecutarLectura();
+
+                if (datos.Lector.Read())
+                {
+                    usuario.Id = (int)datos.Lector["Id"];
+                    usuario.TipoUsuario = (TipoUsuario)(int)datos.Lector["TipoUser"];
+
+                    return true;
+                }
+
+                return false;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+}
